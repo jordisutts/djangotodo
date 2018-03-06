1d2e5cea6e26b986a3bbdcd11fe8133c7681255a
Jordan Sutton <jordan.a.sutton@gmail.com>
2018-03-06 14:55:49 +0000

    todolist

---
+++ todotoday/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TodotodayConfig(AppConfig):
+    name = 'todotoday'
---
+++ todotoday/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from.models import TodoItem
+from .forms import TodoItemForm
+
+# Create your views here.
+
+def get_todo_page(request):
+    if request.method == "POST":
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+        
+    else:
+        print("its the get")
+        
+    form = TodoItemForm()
+    items = TodoItem.objects.all()
+    return render(request, "todo/todo_items.html", {'items':items, 'form':form})  
+    
+    
+def delete_todo_item(request, id):
+    item = get_object_or_404(TodoItem, pk=id)
+    item.delete()
+    return HttpResponse("You deleted " + item.name)
