aae5e5d5c69f36b80e886c4226ec1362ff07c77a
Jordan Sutton <jordan.a.sutton@gmail.com>
2018-03-07 10:21:20 +0000

    added form page

--- todotoday/views.py
+++ todotoday/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from.models import TodoItem
-from .forms import TodoItemForm
+from .forms import TodoItemForm, EditTodoItemForm
 
 
 # Create your views here.
@@ -30,3 +30,19 @@ def toggle_todo_item(request, id):
     item.done = not item.done
     item.save()
     return redirect("/")
+    
+def edit_todo_item(request, id):
+    item = get_object_or_404(TodoItem, pk=id)
+    
+    if request.method == "POST":
+        form = EditTodoItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        print("Get")
+        
+    
+    form = EditTodoItemForm(instance=item)
+    items = TodoItem.objects.all()
+    return render(request, "todo/todo-item-edit.html", {'items':items, 'form':form}) 
